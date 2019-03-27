24835899f9ef79c078ae8a02c4b9c58bb53caa34
papayoun <pierre.gloaguen@gmail.com>
2019-03-27 15:29:47 +0100

    creating benchmark of GEM for future comparison with IS

--- vignettes/example_GEM.R
+++ vignettes/example_GEM.R
@@ -1,18 +1,20 @@
-library(GrandParisPackage)
+# library(GrandParisPackage)
 library(parallel)
-set.seed(122)
+seed <- 122
+set.seed(seed)
 trueTheta <- pi / 4; trueSigma2 <- 1;
 n <- 100; times <- seq(0, to = 50, length = n);
 SINEprocess <- SINE_simulate(trueTheta, trueSigma2, 10, times = times)
 observations <- SINEprocess[, "observations"]
-allRes <- mclapply(1:4, function(i){
+n_case <- 12
+allRes <- mclapply(1:n_case, function(i){
   thetaStart <- runif(1, 0, 2 * pi)
   sigma2Start <- runif(1, 0.2, 2)
   myTry <- GEM(observations = observations, observationTimes = times, thetaStart = thetaStart, 
-               sigma2Start = sigma2Start, nIterations = 6, nModels = 20)
+               sigma2Start = sigma2Start, nIterations = 4, nModels = 20)
   colnames(myTry) = c("theta", "sigma2")
   myTry
-}, mc.cores = detectCores())
+}, mc.cores = min(n_case, detectCores()))
 
 thetaEst <- sapply(allRes, function(x) x[,"theta"]) %% (2 * pi)
 sigmaEst <- sapply(allRes, function(x) x[, "sigma2"])
